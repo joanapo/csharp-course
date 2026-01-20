64d2cd5734214c8989538650687b72b76a4c0488
joana <j.pocopkaite@gmail.com>
2026-01-20 13:03:31 +0000

    upload rocket landing exercise

---
+++ RocketLandingSimulation/RocketLandingSimulation/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string RocketImg = "        |\r\n       / \\\r\n      / _ \\\r\n     |.o '.|\r\n     |'._.'|\r\n     |     |\r\n   ,'|  |  |`.\r\n  /  |  |  |  \\\r\n  |,-'--|--'-.| l42";
+for (int i = 0; i <= 15; i++)
+{
+    Console.Clear();
+    for (int j = 0; j <= i; j++)
+    {
+        Console.WriteLine();
+    }
+    Console.WriteLine(RocketImg);
+    Thread.Sleep(200);
+}
+Console.WriteLine("The rocket has landed. Woohoo! Another successful landing!");
+
+Console.ReadKey();
