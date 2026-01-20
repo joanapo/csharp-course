b021c9bd145662847f43e890c9c80cf033a8211c
joana <j.pocopkaite@gmail.com>
2026-01-20 14:46:45 +0000

    upload 2d array solution

---
+++ 2d-arrays/2d-arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _2d_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    sum += numbers[i, j]; 
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
