fbe419a927b459abd6a7a53b06d272b176938459
joana <j.pocopkaite@gmail.com>
2026-02-02 16:58:54 +0000

    Quiz App

---
+++ MyQuizApp/MyQuizApp/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyQuizApp
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] Answers { get; set; }
+
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question(string questionText, string[] answers, int correctAnswerIndex)
+        {
+            QuestionText = questionText;
+            Answers = answers;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+
+        public bool IsCorrectAnswer(int choice)
+        {
+            return CorrectAnswerIndex == choice;
+        }
+    }
+}
---
+++ MyQuizApp/MyQuizApp/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyQuizApp
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+        private void DisplayQuestion(Question question)
+        {
+            Console.WriteLine(question.QuestionText);
+        }
+    }
+}
