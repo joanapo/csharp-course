cf9571289b4d4744ca1afe9ddce8f1b198304f32
joana <j.pocopkaite@gmail.com>
2026-02-02 14:54:00 +0000

    upload OOP basics

---
+++ ClassesApp/ClassesApp/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesApp
+{
+    internal class Car
+    {
+        // Member variable
+        private string _model = "";
+        private string _brand = "";
+
+        private bool _isLuxury;
+        
+        // Property
+        public string Model { get => _model; set => _model = value; }
+        public string Brand {
+            get
+            {
+                if (_isLuxury)
+                {
+                    return _brand + " - Luxury Edition";
+                }
+                else
+                {
+                    return _brand;
+                }
+            }
+            
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You entered NOTHING!");
+                    _brand = "DEFAULTVALUE";
+                }
+                else
+                {
+                    _brand = value;
+                }
+            }
+        }
+
+        public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+
+        // Constructor
+        public Car(string brand, string model, bool isLuxury)
+        {
+            Brand = brand;
+            Model = model;
+            Console.WriteLine($"A car of the model {Brand} {Model} has been created.");
+            IsLuxury = isLuxury;
+        }
+    }
+}
---
+++ ClassesApp/ClassesApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car("Audi", "A3", false);
+            Car bmw = new Car("BMW", "i7", true);
+            
+            Console.WriteLine("Brand is " + audi.Brand);
+            Console.WriteLine("Brand is " + bmw.Brand);
+
+            Console.ReadKey();
+        }
+    }
+
+}
