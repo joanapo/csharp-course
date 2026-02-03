e32bb4cddd34594331d75630922af8ab3f1a6776
joana <j.pocopkaite@gmail.com>
2026-02-03 16:57:23 +0000

    Lists

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ListsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string>();
+
+            colors.Add("red");
+            colors.Add("blue");
+            colors.Add("green");
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            colors.Remove("red");
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
