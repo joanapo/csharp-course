6dedfecf2f563f07916856b15352a5b65854999d
joana <j.pocopkaite@gmail.com>
2026-02-04 13:03:06 +0000

    Nullable types

---
+++ Nullables/Nullables/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Nullables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age = null;
+
+            if (age.HasValue)
+            {
+                Console.WriteLine("Age is " + age);
+            }
+            else
+            {
+                Console.WriteLine("Age is not specified.");
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
