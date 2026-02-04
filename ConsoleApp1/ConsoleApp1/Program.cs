3e59da68d66681fdbb55a3dabf6eaa405c184621
joana <j.pocopkaite@gmail.com>
2026-02-04 12:13:57 +0000

    List methods

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,13 +4,22 @@
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>
-            {
-                10, 5, 15, 3, 9
-            };
+            List<int> numbers = new List<int>{10, 5, 15, 3, 9, 25, 18};
 
             numbers.Sort();
             foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            bool hasLargeNumber = numbers.Any(x => x > 20);
+            
+            Predicate<int> isGreaterThanTen = x => x > 10;
+            
+            List<int> higherTen = numbers.FindAll(isGreaterThanTen);
+
+            Console.WriteLine("Number 10 and higher in our list:");
+            foreach (int number in higherTen)
             {
                 Console.WriteLine(number);
             }
