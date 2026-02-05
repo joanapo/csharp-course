51e31f42362d7cb471c3695945d8fb297f2e4f18
joana <j.pocopkaite@gmail.com>
2026-02-05 20:34:52 +0000

    Try - Catch - Finally statements

---
+++ TryCatchExceptions/TryCatchExceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TryCatchExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            try
+            {
+                Console.WriteLine("Please enter a number");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = 2;
+                result = num1 / num2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message); 
+            }
+            finally
+            {
+                Console.WriteLine("This always executes.");
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.ReadKey();
+            
+        }
+    }
+}
