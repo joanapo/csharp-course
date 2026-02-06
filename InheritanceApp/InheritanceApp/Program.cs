cf9e83c1ad3ca393295071640714f1add37270d2
joana <j.pocopkaite@gmail.com>
2026-02-06 12:39:47 +0000

    Class inheritance

---
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace InheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Bark();
+            myDog.Eat();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    class Dog: Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+}
