33f589c85fd98588020ae66d83f7e2f959dfdc84
joana <j.pocopkaite@gmail.com>
2026-02-07 17:02:30 +0000

    Multiple inheritance DI

---
+++ InterfaceDependencyInjectionApp/InterfaceDependencyInjectionApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InterfaceDependencyInjectionApp
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    public interface IScannable
+    {
+        void Scan();
+    }
+
+    public class MultiFunctionPrinter : IPrintable, IScannable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing...");
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Scanning...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiFunctionPrinter printer = new MultiFunctionPrinter();
+            printer.Scan();
+            printer.Print();
+            Console.ReadKey();
+        }
+    }
+}
