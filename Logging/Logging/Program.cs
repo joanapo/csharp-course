8edb6401e75a02fcf82425f7cf183a35014f002d
joana <j.pocopkaite@gmail.com>
2026-02-07 14:26:26 +0000

    Creating directories or files on your PC

--- Logging/Logging/Program.cs
+++ Logging/Logging/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            File.AppendAllText("log.txt", "Hello World\n");
+            string directoryPath = @"D:\Logs";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.AppendAllText(filePath, "Hello World\n");
             Console.ReadLine();
         }
     }
