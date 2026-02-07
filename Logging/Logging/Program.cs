2a5d443846811fe62d2db1c9d9f4dee7bb5797a4
joana <j.pocopkaite@gmail.com>
2026-02-07 14:19:18 +0000

    Storing a log file

---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("log.txt", "Hello World\n");
+            Console.ReadLine();
+        }
+    }
+}
