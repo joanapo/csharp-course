f01344b7dc6975edad02020f651bfd8f8bc5249a
joana <j.pocopkaite@gmail.com>
2026-02-07 12:40:48 +0000

    Interfaces

---
+++ InterfacesApp/InterfacesApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InterfacesApp
+{
+    public interface IAnimal
+    {
+        void MakeSound();
+        void Eat(string food);
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
