38cd9d146f7d951670c75844f7ab9b0f6fefa183
joana <j.pocopkaite@gmail.com>
2026-02-08 09:55:00 +0000

    Structs

---
+++ StructsApp/StructsApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StructsApp
+{
+    public struct Point
+    {
+        //public int X { get; set; }
+        //public int Y { get; set; }
+
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Point: ({X}, {Y})");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(10, 20);
+            p1.Display();
+
+            Point p2;
+            p2.X = 10;
+            p2.Y = 20;
+            p2.Display();
+
+            Point p3 = p1;
+            p3.X = 50;
+            p1.Display();
+            p3.Display();
+            
+            Console.ReadKey();
+        }
+    }
+}
