659603de6c7656e31e25a3144f8cf3d91fc74b2d
joana <j.pocopkaite@gmail.com>
2026-02-08 11:03:12 +0000

    Enums

---
+++ EnumApp/EnumApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EnumApp
+{
+    internal class Program
+    {
+        enum Day { Mo, Tu, We, Th, Fr, Sa, Su };
+        enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+        
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day su = Day.Su;
+            Day a = Day.Fr;
+
+            Console.WriteLine(fr==a);
+
+            Console.WriteLine((int)Month.Feb);
+
+            Console.ReadKey();
+        }
+    }
+}
