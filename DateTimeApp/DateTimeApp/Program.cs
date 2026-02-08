e2d224d856482227505edfc09c5ac1e768dc5336
joana <j.pocopkaite@gmail.com>
2026-02-08 12:21:28 +0000

    DateTime

---
+++ DateTimeApp/DateTimeApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DateTimeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2026, 2, 8);
+            Console.WriteLine("Today is {0}", dateTime);
+
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine(DateTime.Now);
+
+            DateTime tomorrow = GetTomorrow();
+            Console.WriteLine("Tomorrow will be: {0}", tomorrow);
+
+            Console.WriteLine("Today is {0}", DateTime.Today.DayOfWeek);
+
+            Console.WriteLine(GetFirstDayofYear(2001));
+
+            int days = DateTime.DaysInMonth(2000, 2);
+            Console.WriteLine("Days in Feb 2000: {0}", days);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Minute: {0}", now.Minute);
+
+            Console.WriteLine("{0} o'clock and {1} minutes and {2} seconds", now.Hour, now.Minute, now.Second);
+
+            Console.WriteLine("Write a date in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+            if (DateTime.TryParse(input, out dateTime)) 
+            {
+                Console.WriteLine(dateTime);
+                TimeSpan daysPassed = now.Subtract(dateTime);
+                Console.WriteLine("Days passed since: {0}", daysPassed.Days);
+            }
+
+            Console.ReadKey();
+        }
+
+        static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        static DateTime GetFirstDayofYear(int year)
+        {
+            return new DateTime(year, 1, 1);
+        }
+    }
+}
