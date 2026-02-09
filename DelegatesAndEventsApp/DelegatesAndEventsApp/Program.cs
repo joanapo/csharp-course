e047c891d2db8b58f3f20285e7f261a188f9fa08
joana <j.pocopkaite@gmail.com>
2026-02-09 12:28:45 +0000

    Generics

---
+++ DelegatesAndEventsApp/DelegatesAndEventsApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DelegatesAndEventsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = {1, 2, 3};
+            string[] stringArray = { "One", "Two", "Three" };
+
+            PrintArray(intArray);
+            PrintArray(stringArray);
+            
+            Console.ReadKey();
+        }
+
+        public static void PrintIntArray(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.WriteLine(i); 
+            }
+        }
+
+        public static void PrintStringArray(string[] array)
+        {
+            foreach(string s in array)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        
+        // a generic method that accepts a generic datatyoe
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (T item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
