dd488ba4fe7620c214b0971d752a84000b2b680d
joana <j.pocopkaite@gmail.com>
2026-02-09 16:13:21 +0000

    Regular expressions

---
+++ RegExApp/RegExApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegExApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there, my number is 12314";
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found", matches.Count);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection group = match.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
