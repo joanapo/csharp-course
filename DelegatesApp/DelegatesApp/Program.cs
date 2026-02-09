d32ae73b49bd6a876e6c15d68fd397c8ba28ac31
joana <j.pocopkaite@gmail.com>
2026-02-09 11:31:56 +0000

    Delegates

---
+++ DelegatesApp/DelegatesApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DelegatesApp
+{
+    internal class Program
+    {
+        // 1. Declaration:
+        public delegate void Notify(string message);
+
+        static void Main(string[] args)
+        {
+            // 2. Instantiation:
+            Notify notifyDelegate = ShowMessage;
+
+            // 3. Invocation
+            notifyDelegate("Hello, Delegates!");
+            
+            
+            Console.ReadKey();
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
