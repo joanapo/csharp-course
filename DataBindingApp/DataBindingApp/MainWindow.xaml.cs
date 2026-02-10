6447e802541001b15ed4ca7dcb71430731518f9d
joana <j.pocopkaite@gmail.com>
2026-02-10 16:22:46 +0000

    Data binding

---
+++ DataBindingApp/DataBindingApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person
+        {
+            Name = "Jannick",
+            Age = 30
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string personData = person.Name + " is " + person.Age + " years old!";
+            MessageBox.Show(personData);
+        }
+    }
+
+}
