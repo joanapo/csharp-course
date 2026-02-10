56b40828f7b365d92253c438fd47158cd998e819
joana <j.pocopkaite@gmail.com>
2026-02-10 16:51:22 +0000

    ListBox

---
+++ ListBoxApp/ListBoxApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBoxApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> People = new List<Person>
+        {
+            new Person{Name = "Jannick", Age = 30},
+            new Person{Name = "Marc", Age = 20},
+            new Person{Name = "Maria", Age = 40},
+            new Person{Name = "Scott", Age = 35},
+            new Person{Name = "Lucas", Age = 27},
+            
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBoxPeople.ItemsSource = People;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListBoxPeople.SelectedItems;
+
+            foreach (var item in selectedItems)
+            {
+                var person = (Person)item; 
+                MessageBox.Show(person.Name);
+            }
+        }
+    }
+}
