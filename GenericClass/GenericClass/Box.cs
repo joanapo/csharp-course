1a86ebf81da5872bbae1948f60854c9cb1bc652f
joana <j.pocopkaite@gmail.com>
2026-04-07 15:43:51 +0100

    Creating a generic class

---
+++ GenericClass/GenericClass/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClass
+{
+    internal class Box<T>
+    {
+        public T Content { get; set; }
+
+        public string Log()
+        {
+            return $"Box contains {Content}";
+        }
+    }
+}
---
+++ GenericClass/GenericClass/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Content = 1;
+            Console.WriteLine(box.Log());
+
+            Box<string> boxStr = new Box<string>();
+            boxStr.Content = "test";
+            Console.WriteLine(boxStr.Log());
+        }
+    }
+}
